3ad88d6f999988dbcc297101fb02bce6ceef2077
gauravbalpande <132254352+gauravbalpande@users.noreply.github.com>
2024-11-15 12:12:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
-const port = process.env.PORT || 80;
+// const port = process.env.PORT || 80;
 
 const app = express();
 const server = require("http").Server(app);
@@ -60,6 +60,6 @@ io.on("connection", (socket) => {
 });
 
 // port listening
-//server.listen(process.env.PORT || 3030, () => {
-  // console.log("server started at port:3030");
-//});
+server.listen(process.env.PORT || 80, () => {
+   console.log("server started at port:3030");
+});
